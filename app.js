2e7c29d5e55e01b60378bacd0ff7ebaf7ae6c847
alinaparhom <alinych.parhom@gmail.com>
2026-03-15 21:27:18 +0300

    Исправить верхний отступ под системную кнопку закрытия Telegram

--- app.js
+++ app.js
@@ -26016,7 +26016,9 @@ function updateTelegramTopControlsOffset() {
     Math.round(currentInnerHeight - viewportStableHeight)
   );
   const hasFullscreenFlag = typeof webApp.isFullscreen === "boolean";
-  const hasSystemCloseButton = hasFullscreenFlag ? webApp.isFullscreen : chromeOffset > 6;
+  const hasSystemCloseButton = hasFullscreenFlag
+    ? !webApp.isFullscreen || chromeOffset > 6
+    : chromeOffset > 6;
 
   let topControlsOffset = 0;
   if (hasSystemCloseButton) {

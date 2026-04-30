58e126a4d4f9b6808bebe123db18b27bc56ed4eb
alinaparhom <alinych.parhom@gmail.com>
2026-04-30 23:40:10 +0300

    Fix no-photo tool tap opening add-photo modal on mobile

--- app.js
+++ app.js
@@ -20091,7 +20091,6 @@ async function setupEnergyDashboard(user, preferences, contextOverride) {
       };
       row.addEventListener("click", openCard);
       row.addEventListener("keydown", openCard);
-      row.addEventListener("touchend", openCard, { passive: false });
 
       row.append(numberCell, infoCell);
       table.appendChild(row);
